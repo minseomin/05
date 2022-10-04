6c3fd66d0eedc335a089caed44f4fe184a5aba82
minseomin <113421574+minseomin@users.noreply.github.com>
2022-10-04 17:19:27 +0900

    04

--- main...c
+++ main...c
@@ -4,18 +4,17 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char c;
-	int num = 0; //숫자를 세는 거니까 숫자변수 하나 선언  
 	
-	printf("문자열을 입력하세요.\n");
-	 
-	while ((c=getchar())!='\n')// 입력문자가 개행문자가 나올 때까지 반복  ... 엔터를 누르는 순간 종료! 엔터가 개행문자가 되는거임  
-	{
-		if(c>='0' && c<='9')
-			num=num+1;	//입력된 글자가 숫자인가? 참이면 num에 하나를 센다.  
-	}
+	int sum = 0;// 누적으로 더할거니까 초기화 필요  
+	int x;
+	int i;
 	
-	printf("숫자의 개수는 %i 개 입니다.\n",num);// num출력
-	 
+	printf("input a number : ");
+	scanf("%d",&x);
+	
+	for (i=0; i<=x ; i++)
+		sum = sum + i ;
+	
+	printf("the result is : %i\n", sum); 
 	return 0;
 }
